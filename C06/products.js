556064febbcca2f5f6dea304ec43e1e28c862f67
DangPhuThanh <dangphuthanh4137@gmail.com>
2025-11-14 13:36:16 +0700

    sua

--- C06/products.js
+++ C06/products.js
@@ -12,13 +12,18 @@ function getProductsFromStorage() {
         if (!products || products.length === 0) {
             console.warn("localStorage 'watchtime_products' trống! Đang tạo dữ liệu mẫu.");
             
+            // js/products.js
+
+// ... (các đoạn code khác giữ nguyên)
+
             // DỮ LIỆU MẪU (Giờ có 10 sản phẩm)
             const defaultProducts = [
                 { 
                   id: "sp_ben10", 
                   name: "BEN 10 OMNITRIX", 
                   price: 999999999, 
-                  image: "../images/ben10.webp", 
+                  // SỬA ĐƯỜNG DẪN TẠI ĐÂY:
+                  image: "images/ben10.webp", 
                   category: "nam", 
                   description_short: "Chỉ dành cho người được chọn...", 
                   description_long: "Chỉ dành cho người được chọn hoặc người có đủ tiền mua."
@@ -27,8 +32,9 @@ function getProductsFromStorage() {
                   id: "sp_conan", 
                   name: "Đồng hồ Conan", 
                   price: 67000000, 
-                  image: "../images/conan.jpg", 
-                  category: "nu", // File gốc của bạn để là 'nu'
+                  // SỬA ĐƯỜNG DẪN TẠI ĐÂY:
+                  image: "images/conan.jpg", 
+                  category: "nu", 
                   description_short: "Trang bị cơ bản của thám tử.", 
                   description_long: "Trang bị cơ bản của thám tử lừng danh. Có thể bắn kim gây mê."
                 },
@@ -36,7 +42,8 @@ function getProductsFromStorage() {
                   id: "sp_doraemon", 
                   name: "Time stop watch", 
                   price: 8500000, 
-                  image: "../images/doraemon.jpg", 
+                  // SỬA ĐƯỜNG DẪN TẠI ĐÂY:
+                  image: "images/doraemon.jpg", 
                   category: "nam", 
                   description_short: "Chất lượng Nhật Bản.", 
                   description_long: "Chất lượng Nhật Bản, bền bỉ với thời gian. Bảo bối của Doraemon."
@@ -45,7 +52,8 @@ function getProductsFromStorage() {
                   id: "sp_oip", 
                   name: "Đồng hồ OIP", 
                   price: 4200000, 
-                  image: "../images/oip.webp", 
+                  // SỬA ĐƯỜNG DẪN TẠI ĐÂY:
+                  image: "images/oip.webp", 
                   category: "doi", 
                   description_short: "Thiết kế hầm hố.", 
                   description_long: "Thiết kế hầm hố, chống va đập tuyệt đối. Đến từ The Amazing World of Gumball."
@@ -54,7 +62,8 @@ function getProductsFromStorage() {
                   id: "sp_timecity", 
                   name: "Đồng hồ Time City", 
                   price: 696500000, 
-                  image: "../images/time_city.webp", 
+                  // SỬA ĐƯỜNG DẪN TẠI ĐÂY:
+                  image: "images/time_city.webp", 
                   category: "doi", 
                   description_short: "Chỉ dành cho giới thượng lưu.", 
                   description_long: "Chỉ dành cho giới thượng lưu. Thiết kế đính kim cương toàn bộ."
@@ -65,7 +74,8 @@ function getProductsFromStorage() {
                   id: "sp_casio_001", 
                   name: "Casio G-Shock GA-2100", 
                   price: 3500000, 
-                  image: "../images/ảnh đồng hồ nam 1.jpg", // (Bạn cần có ảnh này trong thư mục images)
+                  // SỬA ĐƯỜNG DẪN TẠI ĐÂY:
+                  image: "images/ảnh đồng hồ nam 1.jpg", 
                   category: "nam", 
                   description_short: "Thiết kế bát giác mạnh mẽ.", 
                   description_long: "Đồng hồ G-Shock GA-2100 với viền bát giác, mỏng nhẹ và độ bền cao."
@@ -74,7 +84,8 @@ function getProductsFromStorage() {
                   id: "sp_daniel_001", 
                   name: "Daniel Wellington Petite", 
                   price: 4200000, 
-                  image: "../images/ảnh đồng hồ nam 2.jpg", // (Bạn cần có ảnh này trong thư mục images)
+                  // SỬA ĐƯỜNG DẪN TẠI ĐÂY:
+                  image: "images/ảnh đồng hồ nam 2.jpg", 
                   category: "nu", 
                   description_short: "Tinh tế và thanh lịch.", 
                   description_long: "Mẫu DW Petite với mặt đồng hồ nhỏ nhắn, dây lưới sang trọng cho phái nữ."
@@ -83,7 +94,8 @@ function getProductsFromStorage() {
                   id: "sp_seiko_001", 
                   name: "Seiko 5 Sports", 
                   price: 7800000, 
-                  image: "../images/ảnh đồng hồ nam 3.jpg", // (Bạn cần có ảnh này trong thư mục images)
+                  // SỬA ĐƯỜNG DẪN TẠI ĐÂY:
+                  image: "images/ảnh đồng hồ nam 3.jpg", 
                   category: "nam", 
                   description_short: "Cỗ máy cơ tự động bền bỉ.", 
                   description_long: "Dòng Seiko 5 Sports huyền thoại, máy cơ Automatic, khả năng chống nước tốt."
@@ -92,7 +104,8 @@ function getProductsFromStorage() {
                   id: "sp_citizen_001", 
                   name: "Citizen Eco-Drive Pair", 
                   price: 9500000, 
-                  image: "../images/xanhhong.jpg", // (Bạn cần có ảnh này trong thư mục images)
+                  // SỬA ĐƯỜNG DẪN TẠI ĐÂY:
+                  image: "images/xanhhong.jpg", 
                   category: "doi", 
                   description_short: "Đồng hồ cặp năng lượng ánh sáng.", 
                   description_long: "Bộ đôi đồng hồ Citizen sử dụng công nghệ Eco-Drive, không cần thay pin."
@@ -101,11 +114,13 @@ function getProductsFromStorage() {
                   id: "sp_tissot_001", 
                   name: "Tissot Le Locle Nữ", 
                   price: 14500000, 
-                  image: "../images/nhatban.jpg", // (Bạn cần có ảnh này trong thư mục images)
+                  // SỬA ĐƯỜNG DẪN TẠI ĐÂY:
+                  image: "images/nhatban.jpg", 
                   category: "nu", 
                   description_short: "Vẻ đẹp Thụy Sỹ cổ điển.", 
                   description_long: "Đồng hồ Tissot Le Locle Automatic, mặt số la mã, mang đậm phong cách châu Âu."
                 }
+
                 // === KẾT THÚC 5 SẢN PHẨM MỚI ===
             ];
             
